438d51a5e22a3bbb4fd55d4c21d7a105b4a4ae6e
Pedro Moura <pedrorpmoura@gmail.com>
2019-11-09 14:08:23 +0000

    update TPC8

--- TPC8/app/routes/index.js
+++ TPC8/app/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    
+    res.render('index', { title: 'Express' });
 });
+
+
+router.get('/prizes', function(req, res, next) {
+    
+    axios.get('http://localhost:3000/api/prizes')
+        .then(result => {
+            res.render('prizes', { prizes: result.data })
+        })
+        .catch(err => res.render('error', { error: err }));
+})
 
 module.exports = router;
