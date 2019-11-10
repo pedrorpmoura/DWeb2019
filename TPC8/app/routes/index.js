2351ed2fb411d24178597b54c55609a94449635d
Pedro Moura <pedrorpmoura@gmail.com>
2019-11-10 22:13:36 +0000

    update TPC8

--- TPC8/app/routes/index.js
+++ TPC8/app/routes/index.js
@@ -11,11 +11,33 @@ router.get('/', function(req, res, next) {
 
 router.get('/prizes', function(req, res, next) {
     
-    axios.get('http://localhost:3000/api/prizes')
-        .then(result => {
-            res.render('prizes', { prizes: result.data })
+    axios.get('http://localhost:3000/api/prizes',
+        {
+            params: {
+                date: req.query.date,
+                category: req.query.category
+            }
         })
+        .then(result => res.render('prizes', { prizes: result.data }))
         .catch(err => res.render('error', { error: err }));
+})
+
+
+router.get('/prizes/:prizeID', function (req, res, next) {
+
+    const prizeID = req.params.prizeID;
+
+    axios.get('http://localhost:3000/api/prizes/' + prizeID)
+        .then(result => res.render('prize', { prize: result.data[0] }))
+        .catch(err => res.render('error', { error: err }))
+});
+
+
+router.get('/categories', function (req, res, next) {
+
+    axios.get('http://localhost:3000/api/categories')
+        .then(result => res.render('categories', { categories: result.data }))
+        .catch(err => res.render('error', { error: err }))
 })
 
 module.exports = router;
